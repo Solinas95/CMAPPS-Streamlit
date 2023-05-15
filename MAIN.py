b67f9175b6cd348ee3371553fef4b78241639537
Solinas95 <100536871+Solinas95@users.noreply.github.com>
2023-05-15 22:30:06 +0200

    MAIN.py

--- MAIN.py
+++ MAIN.py
@@ -13,8 +13,11 @@ df_train, df_test, df_rul = myfunction.read_data_from_github(url_TRAIN, url_TEST
 df_train, df_test, df_rul = myfunction.remove_nan_columns(df_train, df_test, df_rul)
 
 # RINOMINO COLONNE CON LABELS
-columns = ['unit_ID','time_in_cycles','setting_1', 'setting_2','setting_3','T2','T24','T30','T50','P2','P15','P30','Nf',
-           'Nc','epr','Ps30','phi','NRf','NRc','BPR','farB','htBleed','Nf_dmd','PCNfR_dmd','W31','W32' ]
+columns = ['unit_ID','time_in_cycles','setting_1', 'setting_2','setting_3','T2','T24','T30','T50','P2','P15','P30','Nf','Nc','epr','Ps30','phi','NRf','NRc','BPR','farB','htBleed','Nf_dmd','PCNfR_dmd','W31','W32' ]
+
+sensors = ['T2', 'T24', 'T30', 'T50', 'P2', 'P15', 'P30', 'Nf', 'Nc', 'epr','Ps30', 'phi', 'NRf', 'NRc', 'BPR', 'farB', 'htBleed', 'Nf_dmd','PCNfR_dmd', 'W31', 'W32']
+
+settings = ['setting_1', 'setting_2','setting_3']
 
 df_train, df_test = myfunction.rename_columns(df_train, df_test, columns)
 
@@ -25,6 +28,53 @@ test = myfunction.remove_zero_std_columns(df_test)
 # RIMOZIONE COLONNE CHE NON MI SERVONO ORA
 columns_to_remove = ['setting_1', 'setting_2']
 train, test = myfunction.remove_columns(train, test, columns_to_remove)
+
+# PLOT DEI SENSORI CON STANDARD DEVIATION PIU' ELEVATA
+
+# Ask the user for the unit_id
+selected_unit_id = st.number_input('Please enter a unit ID', min_value=1, value=1)
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def plot_selected_columns(df_train, selected_unit_id, selected_columns, sensors):
+    # Filter the DataFrame for the selected unit ID
+    df_selected_unit = df_train[df_train['unit_ID'] == selected_unit_id]
+    
+    # Define a list of colors
+    colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k']
+    
+    # Calculate the number of rows and columns for the grid
+    num_plots = len(selected_columns)
+    num_cols = int(np.ceil(np.sqrt(num_plots)))
+    num_rows = int(np.ceil(num_plots / num_cols))
+    
+    # Create a figure and a grid of subplots
+    fig, axs = plt.subplots(num_rows, num_cols, figsize=(6*num_cols, 6*num_rows))
+    
+    # Flatten the array of axes, for easier indexing
+    axs = axs.flatten()
+    
+    # Plot each column
+    for i, column in enumerate(selected_columns):
+        axs[i].plot(df_selected_unit[column].values, color=colors[i % len(colors)], label=column)
+        axs[i].set_title('Values of column "{}" for unit ID "{}"'.format(column, selected_unit_id))
+        axs[i].set_xlabel('Count')
+        axs[i].set_ylabel('Value')
+        axs[i].legend()
+    
+    # Remove unused subplots
+    for i in range(num_plots, num_rows*num_cols):
+        fig.delaxes(axs[i])
+    
+    # Adjust the layout so that plots do not overlap
+    plt.tight_layout()
+    
+    # Show the plot
+    plt.show()
+
+plot_selected_columns(df_test, selected_unit_id, sensors, sensors)
 
 ############################# PROVA PRINT TEST PREPROCESS #########################################
 st.write("Preprocessed Test Data:")
