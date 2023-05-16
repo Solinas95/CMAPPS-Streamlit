0d04061a48b2f75a2040ea91d11f109e69339dbe
Solinas95 <100536871+Solinas95@users.noreply.github.com>
2023-05-16 04:17:07 +0200

    myfunction.py

--- myfunction.py
+++ myfunction.py
@@ -201,7 +201,7 @@ def plot_selected_columns(df_train, selected_unit_id, selected_columns):
 
     
     # Create a figure and a grid of subplots
-    fig, axs = plt.subplots(1, 4, figsize=(20, 15))
+    fig, axs = plt.subplots(4, 1, figsize=(20, 50))
     
     # Flatten the array of axes, for easier indexing
     axs = axs.flatten()
