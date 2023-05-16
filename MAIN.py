33307cb1d89d5c350e779cab97be65471762f9fa
Solinas95 <100536871+Solinas95@users.noreply.github.com>
2023-05-16 15:56:15 +0200

    Update MAIN.py

--- MAIN.py
+++ MAIN.py
@@ -122,13 +122,14 @@ if test_data_file is not None:
     
     columns_to_remove=[ "unit_ID" , "time_in_cycles" ]
     df_test_normalized = df_test_normalized.drop(columns_to_remove, axis=1)
-    st.write(df_test_normalized.head())
+    st.write(df_test_normalized.shape)
     sequence_columns = list(df_test_normalized.columns)
-    
-    
+    st.write(sequence_columns)
     # Load the saved model
     model = load_model("model_lstm.h5")
     model.compile(loss='mean_squared_error', optimizer='nadam',metrics=['mae'])
     st.write("model loaded")
+    
+    
     
     
