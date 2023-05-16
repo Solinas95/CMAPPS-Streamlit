5b2e2bebb60d9a987f34b7219461a279c5963622
Solinas95 <100536871+Solinas95@users.noreply.github.com>
2023-05-16 16:44:53 +0200

    Update MAIN.py

--- MAIN.py
+++ MAIN.py
@@ -137,7 +137,9 @@ if test_data_file is not None:
     not_null = result_df[result_df['prediction'].notnull()].copy()
     not_null["prediction"]=not_null["prediction"].astype(int)
     not_null = not_null[not_null['prediction'] >= 30].copy()
-    subset_df = not_null[not_null['prediction'] < 30].copy()
+    subset_df = result_df[result_df['prediction'].notnull()].copy()
+    subset_df["prediction"]=subset_df["prediction"].astype(int)
+    subset_df = [subset_df['prediction'] < 30].copy()
     null = result_df[result_df['prediction'].isnull()].copy()
     null["prediction"]=null["prediction"].fillna('In control')
     
