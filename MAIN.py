06e54cd62c9332590af3c582bd9be1420e978ccd
Solinas95 <100536871+Solinas95@users.noreply.github.com>
2023-05-16 16:47:12 +0200

    Update MAIN.py

--- MAIN.py
+++ MAIN.py
@@ -134,12 +134,18 @@ if test_data_file is not None:
     
     # Assuming you have a DataFrame called df_test
     result_df = myfunction.get_last_sequences_with_predictions(df_test_normalized, sequence_columns , sequence_length, model)
+    
+    
     not_null = result_df[result_df['prediction'].notnull()].copy()
     not_null["prediction"]=not_null["prediction"].astype(int)
     not_null = not_null[not_null['prediction'] >= 30].copy()
+    
+    
     subset_df = result_df[result_df['prediction'].notnull()].copy()
     subset_df["prediction"]=subset_df["prediction"].astype(int)
-    subset_df = [subset_df['prediction'] < 30].copy()
+    subset_df = subset[subset_df['prediction'] < 30].copy()
+    
+    
     null = result_df[result_df['prediction'].isnull()].copy()
     null["prediction"]=null["prediction"].fillna('In control')
     
